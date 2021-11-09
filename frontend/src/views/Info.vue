a9e7c009b2215a581bb506438b73b4931d40a375
wcn4 <bill.neubauer.4@gmail.com>
2021-11-09 18:35:54 -0500

    Modified Info Page

--- frontend/src/views/Info.vue
+++ frontend/src/views/Info.vue
@@ -2,7 +2,8 @@
   <div class="custom-home">
     <main role="main" class="inner cover">
       <h3 class="cover-heading">Info page</h3>
-      <p class="lead">I am going to put some info here eventually, I just need to figure out what that info is.</p>
+      <p class="lead"> This is a demonstration of a website made with Vue. This website was made as a part of Penn State's ACM DevPSU program. </p>
+      <p class="lead"><del> I am going to put some info here eventually, I just need to figure out what that info is. </del> </p>
     </main>
   </div>
 </template>
